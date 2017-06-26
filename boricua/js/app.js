da3960834ce367f876928b27c33045e0a7b10ef8
nato522 <renapl90@gmail.com>
2017-06-26 02:07:10 +0200

    after arrival web

--- boricua/js/app.js
+++ boricua/js/app.js
@@ -1,28 +1,15 @@
-// Set the date we're counting down to
-var countDownDate = new Date("Jun 25, 2017 09:10:00").getTime();
+window.onload = function(){
+  for (var i = 0; i < localStorage.length; i++){
+    var name = localStorage.getItem(localStorage.key(i));
+    document.getElementById(name).checked = true;
+  }
+};
 
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-    // Get todays date and time
-    var now = new Date().getTime();
-
-    // Find the distance between now an the count down date
-    var timeleft = countDownDate - now;
-
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
-
-    // Output the result in an element with id="demo"
-    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
-
-    // If the count down is over, write some text
-    if (timeleft < 0) {
-        clearInterval(x);
-        document.getElementById("countdown").innerHTML = "EXPIRED";
-    }
-}, 1000);
+function checkList(checkbox){
+  if (checkbox.checked){
+    localStorage.setItem(checkbox.name, checkbox.name)
+  }
+  else {
+    localStorage.removeItem(checkbox.name)
+  }
+}
